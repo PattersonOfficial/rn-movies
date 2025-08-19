a89f2f5db804d190bc96507d382061076c95b54e
Benjamin Akrong <akrongwork@gmail.com>
2025-08-19 18:52:10 +0000

    feat: completed tabs component and design

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,6 @@ export default function Index() {
   return (
     <View className='flex-1 items-center justify-center'>
       <Text className='text-2xl font-bold text-emerald-600'>Welcome</Text>
-
-      <Link href='/profile' className='text-emerald-600 mt-3'>
-        Profile
-      </Link>
     </View>
   );
 }
